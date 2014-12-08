b611059b3383fab032fd164166b0eac7b1f588af
Alex Chmykhalo <alex.chmykhalo@vakoms.com.ua>
2014-12-08 13:51:19 +0000

    Added class Crypto. Added method for RSA keypair generation.

---
+++ biomio/protocol/crypt.py
@@ -0,0 +1,18 @@
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+class Crypto:
+
+    @staticmethod
+    def generate_keypair():
+        """
+        Generates RSA keypair.
+        :return: Tuple with private(first) and public (second) keys.
+        """
+        random_generator = Random.new().read
+        key = RSA.generate(1024, random_generator)
+        priv_key = key.exportKey()
+        pub_key = key.publickey().exportKey()
+        print priv_key, pub_key
+        return priv_key, pub_key
