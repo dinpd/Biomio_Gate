e2544adeced8bbd071e2fd34f4a7756c7d3b7659
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2014-12-25 20:29:13 +0200

    Implemented rpc methods to get random pass phrase for symmetric encryption, get and set public pgp key for user by his email/user_id Added new module to store user information into redis.

--- biomio/protocol/rpc/plugins/extension_test_plugin/extension_test_plugin.py
+++ biomio/protocol/rpc/plugins/extension_test_plugin/extension_test_plugin.py
@@ -1,12 +1,12 @@
+import random
+import string
 from yapsy.IPlugin import IPlugin
 
 from biomio.protocol.rpc.rpcutils import biometric_auth
+from biomio.protocol.storage.userinfodatastore import UserInfoDataStore
 
-import tornado.gen
-import time
 
 class ExtensionTestPlugin(IPlugin):
-
     def test_func(self, val1, val2, callback):
         callback({"result": "some value"})
 
@@ -15,3 +15,31 @@ class ExtensionTestPlugin(IPlugin):
         print "Done!"
         callback({"result": "some value"})
         # return {"result": "some value"}
+
+    @biometric_auth
+    def generate_pass_phrase(self, user_id, email, callback):
+        random_pass_pharse = ''.join(
+            random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(16))
+        UserInfoDataStore.instance().store_user_data(user_id=user_id, email=email, pass_phrase=random_pass_pharse)
+        callback({"pass_phrase": random_pass_pharse})
+
+    @biometric_auth
+    def get_pass_phrase(self, user_id, callback):
+        user_pass_phrase = UserInfoDataStore.instance().get_user_data_by_id(user_id=user_id,
+                                                                            key=UserInfoDataStore.PASS_PHRASE_KEY)
+        callback({"pass_phrase": user_pass_phrase})
+
+    @biometric_auth
+    def get_user_public_pgp_key(self, email, callback):
+        user_id = UserInfoDataStore.instance().get_user_id_by_email(email=email)
+        if user_id is None:
+            # TODO Implement functionality that will create a new user and generate public/private key pair
+            pass
+        user_public_pgp_key = UserInfoDataStore.instance().get_user_data_by_id(user_id=user_id,
+                                                                               key=UserInfoDataStore.PUBLIC_PGP_KEY)
+        callback({'public_key': user_public_pgp_key})
+
+    @biometric_auth
+    def store_public_pgp_key(self, user_id, public_pgp_key, callback):
+        UserInfoDataStore.instance().store_user_data(user_id=user_id, email=None, public_pgp_key=public_pgp_key)
+        callback({'result': 'Done'})
---
+++ biomio/protocol/storage/userinfodatastore.py
@@ -0,0 +1,54 @@
+from biomio.protocol.storage.redisstore import RedisStore
+import ast
+
+__author__ = 'andriy.lobashchuk'
+
+
+class UserInfoDataStore(RedisStore):
+    _instance = None
+
+    PASS_PHRASE_KEY = 'pass_phrase'
+    PUBLIC_PGP_KEY = 'public_pgp_key'
+
+    def __init__(self):
+        RedisStore.__init__(self)
+
+    @classmethod
+    def instance(cls):
+        if not cls._instance:
+            cls._instance = UserInfoDataStore()
+
+        return cls._instance
+
+    @classmethod
+    def _redis_user_key(cls, user_id):
+        return 'user:%s' % user_id
+
+    @classmethod
+    def _redis_email_key(cls, email):
+        return 'user_email:%s' % email
+
+    def store_user_data(self, user_id, email=None, **kwargs):
+        user_key = self._redis_user_key(user_id=user_id)
+        self._store_data_dict(key=user_key, **kwargs)
+        if email is not None:
+            email_key = self._redis_email_key(email=email)
+            self._store_data_dict(key=email_key, user_id=user_id)
+
+    def get_user_data_by_id(self, user_id, key=None):
+        redis_key = self._redis_user_key(user_id=user_id)
+        data = self._redis.get(name=redis_key)
+        if not data:
+            data = {}
+        else:
+            data = ast.literal_eval(data)
+        return data.get(key, None) if key is not None else data
+
+    def get_user_id_by_email(self, email):
+        redis_key = self._redis_email_key(email=email)
+        data = self._redis.get(name=redis_key)
+        if not data:
+            data = {}
+        else:
+            data = ast.literal_eval(data)
+        return data.get('user_id', None)
