cbbf1a6b987aa1faa58ab7581be628d52abe06c8
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-01-12 16:35:48 +0200

    Additional parameter fpr gpg, fixes for rpc tests (namespace)

--- biomio/protocol/rpc/plugins/extension_test_plugin/extension_test_plugin.py
+++ biomio/protocol/rpc/plugins/extension_test_plugin/extension_test_plugin.py
@@ -65,8 +65,11 @@ class ExtensionTestPlugin(IPlugin):
         ascii_armored_public_key = None
         ascii_armored_private_key = None
         os.close(fd)
+        if os.environ.get('USERNAME', None) is None:
+            os.environ['USERNAME'] = 'test'
         try:
-            gpg = gnupg.GPG(gnupghome=home, keyring=keyring, options=['--no-default-keyring', '--no-version'])
+            gpg = gnupg.GPG(gnupghome=home, keyring=keyring, options=['--no-default-keyring', '--no-version',
+                                                                      '--no-use-agent'])
             input_data = gpg.gen_key_input(key_type="RSA", key_length=1024, name_comment="Biomio",
                                            name_email=email, passphrase=pass_phrase)
             key = gpg.gen_key(input_data)
