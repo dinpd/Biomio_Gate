4cf96f811a08ab1445a42531cc047bcab843a568
Alex Chmykhalo <alex.chmykhalo@vakoms.com>
2015-02-13 16:20:09 +0000

    rpc_utils.py module: added docstrings and comments.

--- biomio/protocol/rpc/rpcutils.py
+++ biomio/protocol/rpc/rpcutils.py
@@ -8,12 +8,27 @@ from biomio.protocol.settings import settings
 import logging
 logger = logging.getLogger(__name__)
 
+# Arguments that are implicitly passed to every RPC routine.
+# If rpc routine does not takes explicitly one or all of this arguments,
+# it will be excluded from argument list before call. Method should be decorated
+# with @rpc_call or @rpc_call_with_auth decorators for that.
 CALLBACK_ARG = 'callback'
 USER_ID_ARG = 'user_id'
 WAIT_CALLBACK_ARG = 'wait_callback'
 
 
 def _check_rpc_arguments(callable_func, current_kwargs):
+    """
+    Internal helper method to check if all argument should be passed
+    to RPC routine.
+
+    Some arguments (like user_id, wait_callback ...) are implicitly passed
+    to every RPC routine. If rpc routine does not takes explicitly one or
+    all of this arguments, this function exclude them from argument list before call.
+    :param callable_func: RPC routine
+    :param current_kwargs: Named arguments that are passed to RPC routine.
+    :return: Resulting arguments list.
+    """
     result_kwargs = {}
 
     def _get_required_args(callable_func):
@@ -24,9 +39,9 @@ def _check_rpc_arguments(callable_func, current_kwargs):
 
     required_args = _get_required_args(callable_func)
 
-    excluded_params_list = [USER_ID_ARG, CALLBACK_ARG, WAIT_CALLBACK_ARG]
+    implicit_params_list = [USER_ID_ARG, CALLBACK_ARG, WAIT_CALLBACK_ARG]
     for k, v in current_kwargs.iteritems():
-        if k in excluded_params_list and k not in required_args:
+        if k in implicit_params_list and k not in required_args:
             continue
         else:
             result_kwargs[k] = v
@@ -35,6 +50,9 @@ def _check_rpc_arguments(callable_func, current_kwargs):
 
 
 def rpc_call(rpc_func):
+    """
+    Every RPC routine (that do not require biometric authentication) should be decorated with @rpc_call decorator.
+    """
     def _decorator(*args, **kwargs):
         callable_kwargs = _check_rpc_arguments(callable_func=rpc_func, current_kwargs=kwargs)
         result = rpc_func(*args, **callable_kwargs)
@@ -48,6 +66,12 @@ def rpc_call(rpc_func):
 
 @tornado.gen.engine
 def _is_biometric_data_valid(callable_func, callable_args, callable_kwargs):
+    """
+    Provides biometric authentication to support @rpc_call_with_auth
+    :param callable_func: RPC routine
+    :param callable_args: List of arguments of RPC routine.
+    :param callable_kwargs: List of named arguments of RPC routine.
+    """
     user_id = callable_kwargs.get(USER_ID_ARG, None)
     wait_callback = callable_kwargs.get(WAIT_CALLBACK_ARG, None)
     callback = callable_kwargs.get(CALLBACK_ARG, None)
@@ -98,6 +122,9 @@ def _is_biometric_data_valid(callable_func, callable_args, callable_kwargs):
 
 
 def rpc_call_with_auth(rpc_func):
+    """
+    Every RPC routine that require biometric authentication should be decorated with @rpc_call_with_auth decorator.
+    """
     def _decorator(*args, **kwargs):
         _is_biometric_data_valid(callable_func=rpc_func, callable_args=args, callable_kwargs=kwargs)
 
