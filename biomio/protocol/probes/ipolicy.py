25fde028931dd2e528d00a1fa085f1591c1bfef4
Alex Chmykhalo <alex.chmykhalo@vakoms.com>
2015-02-13 17:19:29 +0000

    Added IPolicy interface for future policies implementations. Added FixedOrderPolicy class.

---
+++ biomio/protocol/probes/ipolicy.py
@@ -0,0 +1,20 @@
+
+
+class IPolicy:
+    def __init__(self):
+        pass
+
+    def get_resources_list_for_try(self, available_resources):
+        """
+        Returns list of resources in the correct order, with the corrent number of samples.
+        :param available_resources:
+        :return:
+        """
+        raise NotImplementedError
+
+    def set_config(self, config_string):
+        """
+        Configures policy based on predefined config.
+        :param config_string: String containing configuration for certain policy type.
+        """
+        raise NotImplementedError
---
+++ biomio/protocol/probes/policies/fixedorderpolicy.py
@@ -0,0 +1,11 @@
+
+from biomio.protocol.probes.ipolicy import IPolicy
+
+
+class FixedOrderPolicy(IPolicy):
+    def get_resources_list_for_try(self, available_resources):
+        """
+        Reimplemented from IPolicy.get_resources_list_for_try().
+        """
+        pass
+
