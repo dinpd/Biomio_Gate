7ad173cdfdffff1f037813b8498e0aea2292ba43
Alex Chmykhalo <alex.chmykhalo@vakoms.com>
2015-02-18 18:00:57 +0000

    Refactorings in rpchandler.py class.

--- biomio/protocol/rpc/rpchandler.py
+++ biomio/protocol/rpc/rpchandler.py
@@ -1,4 +1,3 @@
-__author__ = 'alexchmykhalo'
 
 from biomio.protocol.rpc.rpcpluginmanager import RpcPluginManager
 
@@ -7,20 +6,30 @@ logger = logging.getLogger(__name__)
 
 
 class RpcHandler:
+    """
+    RpcHandler class used to handle BIOMIO protocol RPC commands
+    """
     def __init__(self):
         pass
 
     def process_rpc_call(self, user_id, call, namespace, data, wait_callback, callback):
+        """
+        Processes RPC call with the given parameters.
+        :param user_id: User ID string
+        :param call: Name of RPC method.
+        :param namespace: Namespace where given method located.
+        :param data: Dictionary with RPC call parameters.
+        :param wait_callback: Callback, that will be called to notify that additional time for RPC processing is neede
+        :param callback: Callback that receives result of RPC call. Should take two parameters: result -
+        dictionary containing result of RPC method call. status - Status for RPC responce (inprogress, completed, fail)
+        """
         logger.info('Processing RPC call %s/%s, with parameters: %s' % (namespace, call, data))
         rpc_obj = RpcPluginManager.instance().get_rpc_object(namespace=namespace)
 
-        result = None
         if hasattr(rpc_obj, call):
             rpc_call = getattr(rpc_obj, call)
             if rpc_call:
-                result = rpc_call(user_id=user_id, wait_callback=wait_callback, callback=callback, **data)
-
-        return result
+                rpc_call(user_id=user_id, wait_callback=wait_callback, callback=callback, **data)
 
     def get_available_calls(self, namespace):
         return []
