1dd1642c662fbace6363ff1e00200b6a804d178a
Alex Chmykhalo <alex.chmykhalo@vakoms.com>
2015-02-27 16:42:43 +0000

    Added redis-rest.py.

---
+++ redis-rest.py
@@ -0,0 +1,22 @@
+from datetime import date
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+from biomio.protocol.storage.userinfodatastore import UserInfoDataStore
+ 
+class RedisHandler(tornado.web.RequestHandler):
+    def post(self):
+        email = tornado.escape.url_unescape( self.get_argument('email'))
+        user_id = UserInfoDataStore.instance().get_user_id_by_email(email=email)
+        user_data = UserInfoDataStore.instance().get_user_data_by_id(user_id=user_id)
+        del user_data[email]
+        UserInfoDataStore.instance().store_user_data(user_id=user_id, **user_data)
+        self.write(email)
+  
+application = tornado.web.Application([
+    (r"/redis", RedisHandler)
+])
+ 
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
