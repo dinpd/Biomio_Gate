2b41c7c6ae81cda750039d5139ba2bd2a5f014ad
Alex Chmykhalo <alex.chmykhalo@vakoms.com>
2015-02-27 17:14:03 +0000

    Fixed call to get_user_data_by_id()

--- redis-rest.py
+++ redis-rest.py
@@ -11,7 +11,8 @@ class RedisHandler(tornado.web.RequestHandler):
         email = 'andriy.lobashchuk@vakoms.com.ua'
         user_id = UserInfoDataStore.instance().get_user_id_by_email(email=email)
         print user_id
-        user_data = UserInfoDataStore.instance().get_user_data_by_id(user_id=user_id)
+        user_data = UserInfoDataStore.instance().get_user_data_by_id(user_id=user_id,
+                                                                     key=UserInfoDataStore.USER_DATA_KEY)
         print 'user data: ', user_data
         del user_data[email]
         print user_data
