c67661a5c615e4f90042a40f3bcf9ededa7877ae
Alex Chmykhalo <alex.chmykhalo@vakoms.com>
2015-04-22 12:04:44 +0000

    Added docstrings to AuthStateStorage class.

--- biomio/protocol/storage/auth_state_storage.py
+++ biomio/protocol/storage/auth_state_storage.py
@@ -1,5 +1,4 @@
 from biomio.protocol.storage.redis_storage import RedisStorage
-from biomio.protocol.settings import settings
 
 import ast
 
@@ -8,6 +7,9 @@ logger = logging.getLogger(__name__)
 
 
 class AuthStateStorage:
+    """
+    The AuthStateStorage class is responsible for storing current authentication state related information.
+    """
     _instance = None
 
     def __init__(self):
@@ -15,12 +17,22 @@ class AuthStateStorage:
 
     @classmethod
     def instance(cls):
+        """
+        Creates if necessary and returns AuthStateStorage singleton instance.
+        :return: AuthStateStorage singleton instance.
+        """
         if not cls._instance:
             cls._instance = AuthStateStorage()
 
         return cls._instance
 
     def get_probe_data(self, id, key=None):
+        """
+        Gets custom auth state data.
+        :param id: Auth state key id.
+        :param key: Data key that should be retrieved. If None - dictionary containing all data will be retrieved.
+        :return: Auth state data.
+        """
         data = self._persistence_redis.get_data(key=id)
         if not data:
             data = {}
@@ -33,12 +45,29 @@ class AuthStateStorage:
         return data
 
     def store_probe_data(self, id, ttl=None, **kwargs):
+        """
+        Stores custom auth state data.
+        :param id: Auth state key id.
+        :param ttl: Auth state key expiry timeout.
+        :param kwargs: Custom data to store.
+        """
         self._persistence_redis.store_data(key=id, ex=ttl, **kwargs)
 
     def remove_probe_data(self, id):
+        """
+        Removes custom auth state data.
+        :param id: Auth state key id.
+        """
         self._persistence_redis.delete_data(id)
 
     def move_connected_app_data(self, src_key, dst_key, ttl=None):
+        """
+        Moves auth data from source key to destination. Source ky will be removed after moving.
+        Moving performed as one atomic operation.
+        :param src_key: Source key containing data.
+        :param dst_key: Destination key where data should be moved.
+        :param ttl: Auth state key expiry timeout.
+        """
         self._persistence_redis.move_data(src_key=src_key, dst_key=dst_key, ex=ttl)
 
 
