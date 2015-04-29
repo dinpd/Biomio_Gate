8c3df65a672990d2f0bcadc88ec7fb1590d2e7e2
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-04-29 15:50:29 +0300

    Improved error handling.

--- biomio/protocol/data_stores/storage_jobs.py
+++ biomio/protocol/data_stores/storage_jobs.py
@@ -44,25 +44,32 @@ def get_record_job(table_class_name, object_id, callback_code):
     worker_logger.info('Getting record for table class - %s, with object_id - %s' % (table_class_name, object_id))
     try:
         record = MySQLDataStoreInterface.get_object(table_name=table_class_name, object_id=object_id)
-        worker_logger.info('Got record for table class - %s, with object_id - %s' % (table_class_name, object_id))
         worker_logger.debug('Data: %s' % record.to_dict())
         BaseDataStore.instance().store_job_result(record_key=record.get_redis_key(),
                                                   record_dict=record.to_dict(),
                                                   callback_code=callback_code)
     except Exception as e:
-        worker_logger.exception(msg=str(e))
+        worker_logger.exception(e)
+        result = dict(error=str(e))
+        BaseDataStore.instance().store_job_result(record_key=REDIS_DO_NOT_STORE_RESULT_KEY,
+                                                  record_dict=result,
+                                                  callback_code=callback_code)
+    worker_logger.info('Got record for table class - %s, with object_id - %s' % (table_class_name, object_id))
 
 
 def select_records_by_ids_job(table_class_name, object_ids, callback_code):
     worker_logger.info('Getting records for table class - %s, with object_ids - %s' % (table_class_name, object_ids))
     try:
         records = MySQLDataStoreInterface.select_data_by_ids(table_name=table_class_name, object_ids=object_ids)
-        worker_logger.info('Got records for table class - %s, with object_ids - %s' % (table_class_name, object_ids))
         worker_logger.debug('Data: %s' % records)
         BaseDataStore.instance().store_job_result(record_key=REDIS_DO_NOT_STORE_RESULT_KEY % callback_code,
                                                   record_dict=records, callback_code=callback_code)
     except Exception as e:
-        worker_logger.exception(msg=str(e))
+        worker_logger.exception(e)
+        result = dict(error=str(e))
+        BaseDataStore.instance().store_job_result(record_key=REDIS_DO_NOT_STORE_RESULT_KEY % callback_code,
+                                                  record_dict=result, callback_code=callback_code)
+    worker_logger.info('Got records for table class - %s, with object_ids - %s' % (table_class_name, object_ids))
 
 
 def generate_pgp_keys_job(table_class_name, email, callback_code, result_code):
@@ -103,32 +110,47 @@ def generate_pgp_keys_job(table_class_name, email, callback_code, result_code):
 
 def get_probe_ids_by_user_email(table_class_name, email, callback_code):
     worker_logger.info('Getting probe ids by user email - %s' % email)
-    email_data = MySQLDataStoreInterface.get_object(table_name=EMAILS_TABLE_CLASS_NAME, object_id=email)
-    worker_logger.debug('Email Data - %s' % email_data.to_dict())
-    worker_logger.debug(email_data.user)
-    probe_ids = MySQLDataStoreInterface.get_applications_by_user_id_and_type(table_name=table_class_name,
-                                                                             user_id=email_data.user, app_type='probe')
-    worker_logger.debug('probe IDS - %s' % probe_ids)
-    result = dict(result=probe_ids)
-    BaseDataStore.instance().store_job_result(record_key=REDIS_DO_NOT_STORE_RESULT_KEY % callback_code,
-                                              record_dict=result, callback_code=callback_code)
-    worker_logger.info('Got probe ids by user email - %s' % email)
+    result = dict()
+    try:
+        email_data = MySQLDataStoreInterface.get_object(table_name=EMAILS_TABLE_CLASS_NAME, object_id=email)
+        worker_logger.debug('Email Data - %s' % email_data.to_dict())
+        worker_logger.debug(email_data.user)
+        probe_ids = MySQLDataStoreInterface.get_applications_by_user_id_and_type(table_name=table_class_name,
+                                                                                 user_id=email_data.user,
+                                                                                 app_type='probe')
+        worker_logger.debug('probe IDS - %s' % probe_ids)
+        result = dict(result=probe_ids)
+    except Exception as e:
+        worker_logger.exception(e)
+        result = dict(error=str(e))
+    finally:
+        BaseDataStore.instance().store_job_result(record_key=REDIS_DO_NOT_STORE_RESULT_KEY % callback_code,
+                                                  record_dict=result, callback_code=callback_code)
+        worker_logger.info('Got probe ids by user email - %s' % email)
 
 
 def get_extension_ids_by_probe_id(table_class_name, probe_id, callback_code):
     worker_logger.info('Getting extension ids by probe ID - %s' % probe_id)
-    probe_data = MySQLDataStoreInterface.get_object(table_name=table_class_name, object_id=probe_id, return_dict=True)
-    worker_logger.debug('Probe data - %s' % probe_data)
-    user = MySQLDataStoreInterface.get_object(table_name=USERS_TABLE_CLASS_NAME, object_id=probe_data.get('users')[0])
-    worker_logger.debug('USer data - %s' % user.to_dict())
-    extension_ids = MySQLDataStoreInterface.get_applications_by_user_id_and_type(table_name=table_class_name,
-                                                                                 user_id=user,
-                                                                                 app_type='extension')
-    worker_logger.debug('Extension IDS - %s' % extension_ids)
-    result = dict(result=extension_ids)
-    BaseDataStore.instance().store_job_result(record_key=REDIS_DO_NOT_STORE_RESULT_KEY % callback_code,
-                                              record_dict=result, callback_code=callback_code)
-    worker_logger.info('Got extension ids by probe ID - %s' % probe_id)
+    result = dict()
+    try:
+        probe_data = MySQLDataStoreInterface.get_object(table_name=table_class_name, object_id=probe_id,
+                                                        return_dict=True)
+        worker_logger.debug('Probe data - %s' % probe_data)
+        user = MySQLDataStoreInterface.get_object(table_name=USERS_TABLE_CLASS_NAME,
+                                                  object_id=probe_data.get('users')[0])
+        worker_logger.debug('USer data - %s' % user.to_dict())
+        extension_ids = MySQLDataStoreInterface.get_applications_by_user_id_and_type(table_name=table_class_name,
+                                                                                     user_id=user,
+                                                                                     app_type='extension')
+        worker_logger.debug('Extension IDS - %s' % extension_ids)
+        result = dict(result=extension_ids)
+    except Exception as e:
+        worker_logger.exception(e)
+        result = dict(error=str(e))
+    finally:
+        BaseDataStore.instance().store_job_result(record_key=REDIS_DO_NOT_STORE_RESULT_KEY % callback_code,
+                                                  record_dict=result, callback_code=callback_code)
+        worker_logger.info('Got extension ids by probe ID - %s' % probe_id)
 
 
 def update_redis_job():
