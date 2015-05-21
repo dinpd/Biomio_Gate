a67fd268b72bda2015325f4a74f1f0c47fc1226e
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-05-21 19:35:07 +0300

    Fix for import error.

--- biomio/protocol/rpc/app_connection_manager.py
+++ biomio/protocol/rpc/app_connection_manager.py
@@ -1,5 +1,3 @@
-from biomio.protocol.engine import get_app_keys_helper
-
 __author__ = 'alexchmykhalo'
 
 
@@ -22,6 +20,9 @@ class AppConnectionManager():
         :param app_id: Application id for which get connected apps.
         :return: List of connected app ids.
         """
+
+        from biomio.protocol.engine import get_app_keys_helper
+
         apps = get_app_keys_helper(app_id=app_id, extension=extension)
         if apps is not None:
             return apps.get('result', [])
