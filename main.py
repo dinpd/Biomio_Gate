c2765de22c94aabeca6363b1b621a73d305bea2f
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-05-22 16:32:50 +0300

    Changed training REST url argument name.

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
 class InitialProbeRestHandler(tornado.web.RequestHandler):
     def get(self):
-        BioauthFlow.start_training(probe_id=self.get_query_argument('probe_id'), code=self.get_query_argument('code'))
+        BioauthFlow.start_training(probe_id=self.get_query_argument('device_id'), code=self.get_query_argument('code'))
         self.write('<html><head><title>BIOMIO: Initial Probes</title></head><body>'
                    'Please, use BIOMIO probe application to input initial probes. PROBE_ID - %s'
                    '</body></html>' % self.get_query_argument('probe_id'))
