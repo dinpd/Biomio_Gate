35c5f7e936d3ff31554736d7e7ec2522e032a621
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-07-23 15:09:34 +0300

    Modification in Database Clusters Matching

--- biomio/protocol/probes/plugins/face_photo_plugin/algorithms/face/clusters_db_keypoints.py
+++ biomio/protocol/probes/plugins/face_photo_plugin/algorithms/face/clusters_db_keypoints.py
@@ -26,63 +26,67 @@ class ClustersDBMatchingDetector(ClustersMatchingDetector):
         logger.algo_logger.debug("Database loading finished.")
 
     def importSources_Database(self, source):
-        self._database = [dict() for _ in source.keys()]
-        for c_num, item in source.iteritems():
-            item_data = [[] for _ in item.keys()]
-            for d_num, cluster in item.iteritems():
-                desc = [[] for _ in cluster.keys()]
-                for e_num, descriptor in cluster.iteritems():
-                    desc[int(e_num)] = listToNumpy_ndarray(descriptor)
-                item_data[int(d_num)] = desc
-            obj = dict()
-            obj["clusters"] = item_data
-            self._database[int(c_num) - 1] = obj
+
+        def _values(d, key=None):
+            l = sorted(d, key=key)
+            for e in l:
+                yield d[e]
+
+        self._database = [
+            {
+                'clusters': [
+                    [
+                        listToNumpy_ndarray(descriptor) for descriptor in _values(cluster)
+                    ] for cluster in _values(item, key=int)
+                ]
+            } for item in _values(source)
+        ]
 
     def exportSources(self):
         data = self.exportSources_Database()
-        source = dict()
         if len(data.keys()) > 0:
-            source['data'] = data
-            source['threshold'] = self._prob
-        return source
+            return {
+                'data': data,
+                'threshold': self._prob
+            }
+        else:
+            return {}
 
     def exportSources_Database(self):
-        etalon = dict()
-        for i, data in enumerate(self._database):
-            elements = dict()
-            for index, cluster in enumerate(data["clusters"]):
-                desc = dict()
-                if cluster is not None:
-                    for indx, c in enumerate(cluster):
-                        desc[indx] = numpy_ndarrayToList(c)
-                elements[str(index)] = desc
-            etalon[str(i)] = elements
-        return etalon
+        return {
+            str(i): {
+                str(index): {
+                    indx: numpy_ndarrayToList(c) for (indx, c) in enumerate(cluster)
+                    } for (index, cluster) in enumerate(data['clusters']) if cluster is not None
+                } for (i, data) in enumerate(self._database)
+            }
+
+    def _probability(self, matcher, source, test):
+        matches = matcher.knnMatch(listToNumpy_ndarray(test, numpy.uint8),
+                                   listToNumpy_ndarray(source, numpy.uint8), k=1)
+        ms = sum(
+            itertools.imap(
+                lambda v: len(v) >= 1 and v[0].distance < self.kodsettings.neighbours_distance, matches
+            )
+        )
+        return ms / (1.0 * len(matches))
 
     @verifying
     def verify(self, data):
         matcher = Matcher(BruteForceMatcherType)
-        gres = []
+        tprob = 0
         for d in self._database:
-            res = []
+            prob = [0, 0]
             for i, source in enumerate(d['clusters']):
                 test = data['clusters'][i]
                 if (test is None) or (source is None) or (len(test) == 0) or (len(source) == 0):
-                    logger.algo_logger.debug("Cluster #" + str(i + 1) + ": Invalid")
+                    logger.algo_logger.debug("Cluster #%d: Invalid" % (i + 1))
                 else:
-                    matches = matcher.knnMatch(listToNumpy_ndarray(test, numpy.uint8),
-                                               listToNumpy_ndarray(source, numpy.uint8), k=1)
-                    ms = sum(itertools.imap(
-                        lambda v: len(v) >= 1 and v[0].distance < self.kodsettings.neighbours_distance,
-                        matches)
-                    )
-                    prob = ms / (1.0 * len(matches))
-                    res.append(prob * 100)
-                    logger.algo_logger.debug("Cluster #" + str(i + 1) + " (Size: " + str(len(source)) + "): "
-                                             + str(prob * 100) + "%")
-            suma = sum(res)
-            logger.algo_logger.debug("Total for image: " + str(suma / len(res)))
-            gres.append(suma / len(res))
-        s = sum(gres)
-        logger.algo_logger.debug("Total: " + str(s / len(gres)))
-        return s / len(gres)
+                    _prob = 100 * self._probability(matcher, source, test)
+                    prob[0] += _prob
+                    prob[1] += 1
+                    logger.algo_logger.debug("Cluster #%d (Size: %d): %f%%" % (i + 1, len(source), _prob))
+            logger.algo_logger.debug("Total for image: %f%%" % (prob[0]/prob[1]))
+            tprob += (prob[0] / prob[1])
+        logger.algo_logger.debug("Total: %d%%" % (tprob / len(self._database)))
+        return tprob / len(self._database)
