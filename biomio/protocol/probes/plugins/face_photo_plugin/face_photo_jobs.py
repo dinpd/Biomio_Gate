8b3073ea09053a09ef098400707d5336dafabfac
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-08-03 18:01:43 +0300

    Fixed training result update functionality.

--- biomio/protocol/probes/plugins/face_photo_plugin/face_photo_jobs.py
+++ biomio/protocol/probes/plugins/face_photo_plugin/face_photo_jobs.py
@@ -213,7 +213,7 @@ def training_job(images, probe_id, settings, callback_code, try_type, ai_code):
 
         settings.update({'data': image_paths})
         algo_result = AlgorithmsInterface.verification(**settings)
-        if algo_result.get('status', '') == "update":
+        if isinstance(algo_result, dict) and algo_result.get('status', '') == "update":
             # record = dictionary:
             # key          value
             #      'status'     "update"
@@ -225,18 +225,40 @@ def training_job(images, probe_id, settings, callback_code, try_type, ai_code):
             # algoID if it doesn't exists
             database = algo_result.get('database', None)
             if database is not None:
-                training_data = base64.b64encode(cPickle.dumps(database, cPickle.HIGHEST_PROTOCOL))
-                try:
-                    MySQLDataStoreInterface.create_data(table_name=TRAINING_DATA_TABLE_CLASS_NAME, probe_id=probe_id,
-                                                        data=training_data)
-                except Exception as e:
-                    if '1062 Duplicate entry' in str(e):
-                        worker_logger.info('Training data already exists, updating the record.')
-                        MySQLDataStoreInterface.update_data(table_name=TRAINING_DATA_TABLE_CLASS_NAME,
-                                                            object_id=probe_id, data=training_data)
-                    else:
-                        worker_logger.exception(e)
+                _store_training_db(database, probe_id)
                 result = True
+        elif isinstance(algo_result, list):
+            for algo_result_item in algo_result:
+                if algo_result_item.get('status', '') == "error":
+                    worker_logger.exception('Error during education - %s, %s, %s' % (algo_result_item.get('status'),
+                                                                                     algo_result_item.get('type'),
+                                                                                     algo_result_item.get('details')))
+                    ai_response_type.update({'status': 'error'})
+                    if 'Internal Training Error' in algo_result_item.get('type', ''):
+                        error = algo_result_item.get('details', {}).get('message', '')
+                elif algo_result_item.get('status', '') == 'update':
+                    database = algo_result_item.get('database', None)
+                    if database is not None:
+                        _store_training_db(database, probe_id)
+            # record = dictionary:
+            # key          value
+            #      'status'     "error"
+            #      'type'       Type of error
+            #      'userID'     Unique user identificator
+            #      'algoID'     Unique algorithm identificator
+            #      'details'    Error details dictionary
+            #
+            # Algorithm can have three types of errors:
+            #       "Algorithm settings are empty"
+            #        in this case fields 'userID', 'algoID', 'details' are empty
+            #       "Invalid algorithm settings"
+            #        in this case 'details' dictionary has following structure:
+            #               key         value
+            #               'params'    Parameters key ('data')
+            #               'message'   Error message (for example "File <path> doesn't exists")
+            #       "Internal algorithm error"
+            # Need save to redis
+            pass
         elif algo_result.get('status', '') == "error":
             worker_logger.exception('Error during education - %s, %s, %s' % (algo_result.get('status'),
                                                                              algo_result.get('type'),
@@ -295,3 +317,17 @@ def training_job(images, probe_id, settings, callback_code, try_type, ai_code):
                 worker_logger.error('Failed to build rest request to AI - %s' % str(e))
                 worker_logger.exception(e)
     worker_logger.info('training finished for user - %s, with result - %s' % (settings.get('userID'), result))
+
+
+def _store_training_db(database, probe_id):
+    training_data = base64.b64encode(cPickle.dumps(database, cPickle.HIGHEST_PROTOCOL))
+    try:
+        MySQLDataStoreInterface.create_data(table_name=TRAINING_DATA_TABLE_CLASS_NAME, probe_id=probe_id,
+                                            data=training_data)
+    except Exception as e:
+        if '1062 Duplicate entry' in str(e):
+            worker_logger.info('Training data already exists, updating the record.')
+            MySQLDataStoreInterface.update_data(table_name=TRAINING_DATA_TABLE_CLASS_NAME,
+                                                object_id=probe_id, data=training_data)
+        else:
+            worker_logger.exception(e)
