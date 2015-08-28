ae0afe7e20ead626f088e2dd5fcba4576a326657
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-08-28 17:08:02 +0300

    Fixing issue with redis worker connections.

--- biomio/worker/worker_interface.py
+++ biomio/worker/worker_interface.py
@@ -5,7 +5,7 @@ from threading import Lock
 from os import urandom
 from hashlib import sha1
 
-from redis import Redis
+from redis.client import StrictRedis
 
 from rq import Queue
 
@@ -24,7 +24,7 @@ class WorkerInterface:
     _lock = Lock()
 
     def __init__(self):
-        self._queue = Queue(connection=Redis(host=settings.redis_host, port=settings.redis_port))
+        self._queue = Queue(connection=StrictRedis(host=settings.redis_host, port=settings.redis_port))
         self._subscribed_callbacks = dict()
         self._redis_client = Client(host=settings.redis_host, port=settings.redis_port)
         self._redis_probes_client = Client(host=settings.redis_host, port=settings.redis_port)
--- worker.py
+++ worker.py
@@ -1,16 +1,16 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+from redis.client import StrictRedis
 
 from rq import Connection, Queue
 from rq_gevent_worker import GeventWorker
-from redis import Redis
 from biomio.protocol.settings import settings
 from logger import worker_logger
 
 if __name__ == '__main__':
     # Tell rq what Redis connection to use
     with Connection():
-        q = Queue(connection=Redis(host=settings.redis_host, port=settings.redis_port))
+        q = Queue(connection=StrictRedis(host=settings.redis_host, port=settings.redis_port))
         gevent_worker = GeventWorker(q)
         gevent_worker.log = worker_logger
         gevent_worker.work()
