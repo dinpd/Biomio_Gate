b16c4c011d77c3d05299025852062f68f6024b4b
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-09-10 16:26:40 +0300

    Added auth_code input parameter for AI.

--- biomio/protocol/rpc/plugins/auth_client_plugin/auth_client_plugin.py
+++ biomio/protocol/rpc/plugins/auth_client_plugin/auth_client_plugin.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 class AuthClientPlugin(IPlugin):
 
     @rpc_call_with_auth
-    def process_auth(self):
+    def process_auth(self, auth_code):
         logger.info('Authentication was successful.')
+        # TODO: POST auth code to AI here.
         return dict(resut=True)
