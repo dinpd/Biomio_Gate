19ff097e054c78adaf73a1069eaacc7a56561be6
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-11-04 18:02:31 +0200

    Removed push notification badge.

--- biomio/utils/utils.py
+++ biomio/utils/utils.py
@@ -29,7 +29,7 @@ def push_notification_callback(message):
                     cert_file = APNS_PRODUCTION_PEM
 
                 apns = APNs(use_sandbox=settings.dev_mode, cert_file=cert_file)
-                payload = Payload(alert=message, sound='default', badge=1)
+                payload = Payload(alert=message, sound='default')
 
                 apns.gateway_server.send_notification(token_hex=push_token, payload=payload)
                 logger.info('Notification sent')
