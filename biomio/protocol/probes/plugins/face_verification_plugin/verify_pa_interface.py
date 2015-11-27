dd59e505b16c51b9f22096cf2177042328cf66e1
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-11-27 15:25:38 +0200

    Added verification plugin and base interface for parallel and sequential verification process

---
+++ biomio/protocol/probes/plugins/face_verification_plugin/verify_pa_interface.py
@@ -0,0 +1,36 @@
+from biomio.algorithms.interfaces import AlgorithmInterface
+from biomio.algorithms.recognition.processes import (MainTrainingProcess, TrainingProcess, DataDetectionProcess,
+                                                     RotationDetectionProcess, RotationResultProcess,
+                                                     ClusterMatchingProcess, FinalTrainingProcess)
+from biomio.worker.worker_interface import WorkerInterface
+from defs import TEMP_DATA_PATH
+
+
+class VerificationPAInterface(AlgorithmInterface):
+    def __init__(self):
+        pass
+
+    def training(self, callback=None, **kwargs):
+        worker = WorkerInterface.instance()
+        main_process = MainTrainingProcess(TEMP_DATA_PATH, worker)
+        training_process = TrainingProcess(TEMP_DATA_PATH, worker)
+        data_detect_process = DataDetectionProcess(TEMP_DATA_PATH, worker)
+        rotation_detect_process = RotationDetectionProcess(TEMP_DATA_PATH)
+        rotation_result_process = RotationResultProcess(TEMP_DATA_PATH, worker)
+        cluster_matching_process = ClusterMatchingProcess(worker)
+        final_training_process = FinalTrainingProcess()
+
+        main_process.set_data_training_process(training_process)
+        training_process.set_data_detection_process(data_detect_process)
+        training_process.set_data_rotation_process(rotation_detect_process)
+        rotation_detect_process.set_rotation_result_process(rotation_result_process)
+        rotation_result_process.set_data_detection_process(data_detect_process)
+        data_detect_process.set_cluster_matching_process(cluster_matching_process)
+        data_detect_process.set_final_training_process(final_training_process)
+        cluster_matching_process.set_final_training_process(final_training_process)
+
+        main_process.process(kwargs["data"])
+
+
+    def apply(self, callback=None, **kwargs):
+        pass
---
+++ biomio/protocol/probes/plugins/face_verification_plugin/verify_sa_interface.py
@@ -0,0 +1,5 @@
+from biomio.algorithms.interfaces import AlgorithmInterface
+
+class VerificationSAInterface(AlgorithmInterface):
+    def __init__(self):
+        pass
