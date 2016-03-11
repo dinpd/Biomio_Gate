a0dbb92734b1000724a22e1ab2bbf38d8995e077
maksym.lynnyk <maksym.lynnyk@vakoms.com.ua>
2016-03-11 18:27:32 +0200

    Vagrant settings fix for OpenCV library

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,6 +2,7 @@
 
 PPA_REPOSITORIES=(
     'ppa:jon-severinsson/ffmpeg'
+    'ppa:george-edison55/cmake-3.x'
 )
 
 PKG_DEPENDENCIES=(
@@ -18,6 +19,36 @@ PKG_DEPENDENCIES=(
     'mysql-server'
     'libmysqlclient-dev'
     'mc'
+    'cmake'
+    'unzip'
+    'pkg-config'
+    'checkinstall'
+    'yasm'
+    'python-numpy'
+    'python-opencv'
+    'libopencv-dev'
+    'libavcodec-dev'
+    'libavformat-dev'
+    'libswscale-dev'
+    'libjpeg-dev'
+    'libpng-dev'
+    'libtiff5-dev'
+    'libjasper-dev'
+    'libgtk2.0-dev'
+    'libdc1394-22-dev'
+    'libtbb2'
+    'libtbb-dev'
+    'libqt4-dev'
+    'libxine2-dev'
+    'libgstreamer0.10-dev'
+    'libgstreamer-plugins-base0.10-dev'
+    'libv4l-dev'
+    'libfaac-dev'
+    'libmp3lame-dev'
+    'libtheora-dev'
+    'libvorbis-dev'
+    'v4l-utils'
+    'x264'
 )
 
 # Enable multiverse.
@@ -59,10 +90,16 @@ fi
 
 # Install OpenCV
 pushd /tmp
-    git clone https://github.com/jayrambhia/Install-OpenCV.git
-    pushd Install-OpenCV/Ubuntu/
-        ./opencv_latest.sh
-    popd
+    wget http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.11/opencv-2.4.11.zip
+    unzip opencv-2.4.11.zip
+    cd opencv-2.4.11
+    mkdir build
+    cd build
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+    make -j 4
+    sudo make install
+    sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+    sudo ldconfig
 popd
 
 cp /vagrant/ssh_config /etc/ssh
@@ -109,4 +146,3 @@ mysql $DB < /vagrant/schema.sql
 
 # Change binding address for MySQL
 sed -i -r 's/bind-address\t\t= 127.0.0.1/bind-address\t\t= 0.0.0.0/' /etc/mysql/my.cnf
-
